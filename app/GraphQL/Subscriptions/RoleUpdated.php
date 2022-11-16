78752f263b828dd8b7f88e4590b58d0b25467c5b
xorinzor <4212335+xorinzor@users.noreply.github.com>
2022-11-16 23:25:48 +0100

    implemented subscription-based user & guest permissions updating

--- app/GraphQL/Subscriptions/RoleUpdated.php
+++ app/GraphQL/Subscriptions/RoleUpdated.php
@@ -3,6 +3,7 @@
 namespace App\GraphQL\Subscriptions;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use Nuwave\Lighthouse\Schema\Types\GraphQLSubscription;
 use Nuwave\Lighthouse\Subscriptions\Subscriber;
 
--- routes/api.php
+++ routes/api.php
@@ -18,24 +18,12 @@ Route::pattern('uuid', '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]
 
 /*
  * --------------------------------------------------------------------------
- * These routes are always available
- * --------------------------------------------------------------------------
- */
-Route::get('role/guest', 'RoleApiController@guest');
-Route::get('permission/user', 'PermissionApiController@user');
-
-/*
- * --------------------------------------------------------------------------
  * Routes within this group require to be authenticated
  * --------------------------------------------------------------------------
  */
 Route::group(
     ['middleware' => 'auth:sanctum'],
     function () {
-        Route::get('system/health/', 'SystemApiController@getHealthStatus')->middleware('can:admin.healthcheck');
-        Route::post('system/health/fix', 'SystemApiController@fixHealth')->middleware('can:admin.healthcheck.fix');
-
         Route::post('upload', 'UploadApiController@store')->middleware('can:website.upload');
-        Route::get('role/user', 'RoleApiController@user');
     }
 );
